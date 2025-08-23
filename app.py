0eaf993800ed1631dd460fbe5352941fc31fcb00
AnthonyMacks <56456892+AnthonyMacks@users.noreply.github.com>
2025-08-23 19:13:22 +1000

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ users = {
 
 @auth.verify_password
 def verify_password(username,password):
-    if username in users and check_password_has h(users.get(username),password):
+    if username in users and check_password_hash(users.get(username), password):
     return username
 
 @app.route('/')
@@ -87,4 +87,5 @@ signal.signal(signal.SIGTERM, handle_sigterm)
 if __name__ == "__main__":
     PORT = int(os.environ.get("PORT", 10000))  # Render binds to $PORT
     socketio.run(app, host="0.0.0.0", port=PORT)
+
 
