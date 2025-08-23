772328ab88af5b159cd154139cfc5fcdcf253b8f
AnthonyMacks <56456892+AnthonyMacks@users.noreply.github.com>
2025-08-23 20:12:22 +1000

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,10 @@ def verify_password(username,password):
     if username in users and check_password_hash(users.get(username),password):
         return username
 
+@app.route('/')
 @auth.login_required
 def index():
-    return "Hello, you are logged in!."
+    return render_template("map.html")
 
 socketio = SocketIO(app, async_mode="eventlet", cors_allowed_origins="*")
 
